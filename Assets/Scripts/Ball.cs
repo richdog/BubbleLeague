e0dd5c9aff9057a5dc9319f5dce92bf2c53e464a
Rich <richb567@gmail.com>
2025-01-25 12:51:55 -0500

    ball improvements, buoyancy

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using static Player;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
+    [SerializeField]
+    private Rigidbody _rigidbody;
 
     private MatchManager.Team? _owningTeam;
+
+    [SerializeField, Range(0, 2)]
+    private float waterDragModifier = 0.5f;
+
+    private bool _isUnderwater = false;
+
+    [SerializeField, Range(0, 2)] private float _buoyancy = 1;
 
     public void RespawnBall()
     {
@@ -29,5 +39,34 @@ public class Ball : MonoBehaviour
     {
         Debug.Log("Team " + team + " has claimed the ball");
         _owningTeam = team;
+    }
+
+    public void FixedUpdate()
+    {
+        if (_rigidbody != null)
+        {
+            if(_isUnderwater)
+                _rigidbody.AddForce(Physics.gravity * -_buoyancy);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var water = other.GetComponent<Water>();
+        if (water != null)
+        {
+            _isUnderwater = true;
+            _rigidbody.linearDamping = water.WaterDrag * waterDragModifier;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var water = other.GetComponent<Water>();
+        if (water != null)
+        {
+            _isUnderwater = false;
+            _rigidbody.linearDamping = 0;
+        }
     }
 }
