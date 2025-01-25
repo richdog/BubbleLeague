be4eadb8e8fb68f87f3c0a27a29de8820fcd3e13
IRSMsoso <cameronvandertuig@proton.me>
2025-01-24 21:22:28 -0500

    Add MatchManager, StageOneGoal, and Ball

---
+++ Assets/Scripts/StageOneGoal.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class StageOneGoal : MonoBehaviour
+{
+    [SerializeField] private MatchManager.Team goalTeam = MatchManager.Team.Team1;
+    private BoxCollider _boxCollider;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var ball = collision.gameObject.GetComponent<Ball>();
+
+        if (!ball) return;
+
+        if (MatchManager.Instance.GiveTeamAdvantage(goalTeam))
+        {
+            // Consume ball and shoot it out of top vent here
+        }
+    }
+}
