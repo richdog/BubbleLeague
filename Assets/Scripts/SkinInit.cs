cb16d665f6b85ce88b41ca5dc935a77731b83c6e
IRSMsoso <cameronvandertuig@proton.me>
2025-01-25 21:14:21 -0500

    Fix Skinning of Penguins

--- Assets/Scripts/SkinInit.cs
+++ Assets/Scripts/SkinInit.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SkinInit: MonoBehaviour
+public class SkinInit : MonoBehaviour
 {
     public Player player;
     public SpriteRenderer bodyRender;
@@ -15,7 +15,7 @@ public class SkinInit: MonoBehaviour
 
     public Sprite p3Body;
     public Sprite p3Wing;
-    
+
     public Sprite p4Body;
     public Sprite p4Wing;
 
@@ -26,27 +26,27 @@ public class SkinInit: MonoBehaviour
 
     public void SetPlayerSkin(int id)
     {
-        switch(id)
+        switch (id)
         {
-            case 1:
+            case 0:
                 bodyRender.sprite = p1Body;
                 leftWindRender.sprite = p1Wing;
-                rightWindRender.sprite= p1Wing;
+                rightWindRender.sprite = p1Wing;
                 break;
 
-            case 2:
+            case 1:
                 bodyRender.sprite = p2Body;
                 leftWindRender.sprite = p2Wing;
                 rightWindRender.sprite = p2Wing;
                 break;
 
-            case 3:
+            case 2:
                 bodyRender.sprite = p3Body;
                 leftWindRender.sprite = p3Wing;
                 rightWindRender.sprite = p3Wing;
                 break;
 
-            case 4:
+            case 3:
                 bodyRender.sprite = p4Body;
                 leftWindRender.sprite = p4Wing;
                 rightWindRender.sprite = p4Wing;
